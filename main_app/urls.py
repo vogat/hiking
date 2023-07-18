dd50780ba550b0403c6f8bb4bed03ff1e3c9104c
vogat <james.voight@me.com>
2023-07-18 10:41:03 -0400

    final

---
+++ main_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('trails/', views.trails_index, name='index'),
+  path('trails/<int:trail_id>/', views.trails_detail, name='detail'),
+  path('trails/create/', views.TrailCreate.as_view(), name='trails_create'),
+  path('trails/<int:pk>/update/', views.TrailUpdate.as_view(), name='trails_update'),
+  path('trails/<int:pk>/delete/', views.TrailDelete.as_view(), name='trails_delete'),
+]
